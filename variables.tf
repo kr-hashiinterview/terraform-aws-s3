1b665ba2c0140301ceedd510f6a7998416927c93
kr-hashiinterview <kr.hashiinterview@gmail.com>
2025-03-26 06:18:48 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@
 variable "region" {
   description = "AWS region for all resources."
   type        = string
-  default     = "us-east-1"
+  default     = "eu-central-1"
 }
 
 variable "bucket_name" {
