38cceb8c7fbbcd9507e997309c489f4091e507c4
kr-hashiinterview <kr.hashiinterview@gmail.com>
2025-03-26 05:54:30 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,15 +4,10 @@
 variable "region" {
   description = "AWS region for all resources."
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
-variable "db_name" {
-  description = "Unique name to assign to RDS instance."
-  type        = string
-}
-
-variable "db_username" {
-  description = "RDS root username."
+variable "bucket_name" {
+  description = "Unique name to assign to bucket."
   type        = string
 }
